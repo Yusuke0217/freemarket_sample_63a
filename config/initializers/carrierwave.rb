8b16eb2470b1961706556d7c56f0f2920ad78127
Akihide Tsue <akihide.tsue@gmail.com>
2019-11-22 14:17:39 +0900

    S3の条件分岐追記
    # what
    carrierwaveの開発・テストと本番での保存先変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,9 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -14,4 +17,5 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'freemarket63a'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket63a'
+  end
 end
