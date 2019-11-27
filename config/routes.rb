c3fb7a7f867cf1ec239dd250141a141d754218bd
Akihide Tsue <akihide.tsue@gmail.com>
2019-11-27 21:16:58 +0900

    route修正commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # root to: "home#top"
-
-  #view確認用ルーティング
-  # root to: "mypage#user_info_register"
-  root to: "mypage#profile"
-
-  end
+  root to: "home#top"
+end
