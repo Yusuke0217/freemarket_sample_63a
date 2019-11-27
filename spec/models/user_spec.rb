cb6ae576e5a398d403a2ce48fb70ea0caac403fe
Akihide Tsue <akihide.tsue@gmail.com>
2019-11-27 14:46:50 +0900

    デバイスBI
    # what
    userモデル作成
    テストは最後にする

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe User do
+  describe '#create' do
+    it "is invalid without an email" do
+    end
+  end
+end
+describe User do
+  describe '#create' do
+    it "is invalid without a password" do
+    end
+  end
+end
