99710316a56d4a116cbec1cc2656aa51e74f7f8e
chiro7yu <yuprotec7@gmail.com>
2019-12-03 19:53:12 +0900

    db保存完了、user_idと制約を仮で削除中

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new (product_params)
+    @product.save
     redirect_to root_path
     # if @product.save
     #   redirect_to root_path, notice: 'OK'
