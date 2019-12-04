adfdc829271a9ac572b2ca6cc93848b516fb5019
chiro7yu <yuprotec7@gmail.com>
2019-12-04 17:30:48 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
   # has_many :comments
   # has_many :images
   
-  validates :image, :text, :name, :price, :condition, :shipping_charge, :shipping_how, :shipping_date, :shipping_place, presence: true, unless: :image?
+  validates :text, :name, :price, :condition, :shipping_charge, :shipping_how, :shipping_date, :shipping_place, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
 end
