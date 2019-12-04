2db529567c39083ea57ec17c76477ed1f4729293
h226 <nohmi.a.number14@gmail.com>
2019-12-04 16:01:26 +0900

    up

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
-  mount_uploader :image, ImageUploader
-
+  
   belongs_to :user
   has_many :comments
-  has_many :photos
-
-  validates :image, :text, :size, :name, :price, :condition, :shipping_charge, :shipping_how, :shipping_date, :shipping_place, presence: true
+  has_many :images
+  
+  validates :image, :text, :name, :price, :condition, :shipping_charge, :shipping_how, :shipping_date, :shipping_place, presence: true
+  mount_uploader :image, ImageUploader
 end
