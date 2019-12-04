75d332fe8961824cb32cb183c13f73daf3bdb1c3
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2019-12-03 22:32:16 -0800

    association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  belongs_to :user
+  has_many :comments
+  has_many :photos
+
+  validates :image, :text, :size, :name, :price, :condition, :shipping_charge, :shipping_how, :shipping_date, :shipping_place, presence: true
 end
