b1541e0048ed11b6614fc1fdf969015bb0fd9d20
hamotaro <tetsuya1329@gmail.com>
2019-12-06 00:56:15 +0900

    for ツエさん

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -17,7 +17,7 @@ document.addEventListener(
             $("#cvc").removeAttr("name");
             $("#exp_month").removeAttr("name");
             $("#exp_year").removeAttr("name"); //データを自サーバにpostしないように削除
-            $("#card_token").append(
+            $("#token_submit").append(
               $('<input type="hidden" name="payjp-token">').val(response.id)
             ); //取得したトークンを送信できる状態にします
             $("#inputForm").submit();
