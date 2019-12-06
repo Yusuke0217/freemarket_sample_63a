ae46024f4953571ad965023875f0992a01a8bdb0
Akihide Tsue <akihide.tsue@gmail.com>
2019-12-06 19:51:11 +0900

    デプロイ変更しました

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.178.215.229', user: 'ec2-user', roles: %w{app db web}
+server '18.176.246.240', user: 'ec2-user', roles: %w{app db web}
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
 
