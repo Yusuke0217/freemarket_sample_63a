7262cbe3c62d05eb120a89f6f03c7b227d583a5e
h226 <nohmi.a.number14@gmail.com>
2019-12-06 13:04:04 +0900

    商品編集、削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/mypage/logout', to: 'mypage#logout'
   # get '/mypage/user_page', to: 'mypage#user_page'
   root to: "products#index"
-  resources :products, only: [:index, :new, :create, :show] do
+  resources :products do
     member do
       get :myproduct
       get :my_product_detail
