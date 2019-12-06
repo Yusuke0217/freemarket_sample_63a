7e093a1ce1b86c5b6f055f3325e70017c65edb36
hamotaro <tetsuya1329@gmail.com>
2019-12-06 12:23:49 +0900

    カード登録完了

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -28,7 +28,7 @@ class CardController < ApplicationController
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
-      # redirect_to done_signup_index_path
+      redirect_to done_signup_index_path
     end
   end
 
