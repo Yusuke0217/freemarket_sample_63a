4a35a38bac7a26b0ab6ce718e7c6272af598e302
hamotaro <tetsuya1329@gmail.com>
2019-12-09 15:21:04 +0900

    パンくず親設定

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -5,6 +5,30 @@ end
 crumb :show do
   link "マイページ", user_path
 end
+
+crumb :myproduct do
+  link "出品した商品-出品中", myproduct＿product_path
+  parent :show
+end
+
+crumb :profile do
+  link "プロフィール", profile_user_path
+  parent :show
+end
+
+crumb :user_info_register do
+  link "本人情報の登録", user_info_register_user_path
+  parent :show
+end
+
+crumb :logout do
+  link "ログアウト", logout_user_path
+  parent :show
+end
+
+# crumb : do
+#   link "支払い方法", _user_path
+# end
 # crumb :projects do
 #   link "Projects", projects_path
 # end
