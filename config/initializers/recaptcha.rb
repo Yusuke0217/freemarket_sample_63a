736a36ccd00f5b2f319e743e0bfa9cad9e0ee6ac
Akihide Tsue <akihide.tsue@gmail.com>
2019-12-10 13:30:11 +0900

    recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,5 @@
 Recaptcha.configure do |config|
+  RECAPTCHA_PUBLIC_KEY = '6LfZzMYUAAAAADcwjGrz9bgGHvplolW1sQ3NAZLr'
   config.site_key = ENV['RECAPTCHA_PUBLIC_KEY']
   config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
 end
