e2ce6be6d404b10573ad9b519f7a93b7042c426e
h226 <nohmi.a.number14@gmail.com>
2019-12-11 12:05:51 +0900

    サイドバー修正、発送日数のView崩れ修正

--- db/migrate/20191202112135_create_products.rb
+++ db/migrate/20191202112135_create_products.rb
@@ -11,7 +11,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string   :shipping_charge, null: false
       t.string   :shipping_how
       t.string   :shipping_place,  null: false
-      t.integer  :shipping_date,   null: false
+      t.string  :shipping_date,   null: false
       t.integer  :price,           null: false
       t.string   :text,            null: false
       t.text     :image,           null: false
